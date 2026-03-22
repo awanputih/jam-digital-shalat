fc3ca51fc4bffe3cd2d96a13429a52a278303398
awanputih <awanputih0805@gmail.com>
2026-03-22 13:02:21 +0700

    Push main.cpp for ESP8266 firmware on 2026-03-22

---
+++ firmware-esp8266/src/main.cpp
@@ -0,0 +1,11 @@
+// Your ESP8266 firmware code goes here
+#include <ESP8266WiFi.h>
+
+void setup() {
+    // Initialize WiFi
+    WiFi.begin("yourSSID", "yourPASSWORD");
+}
+
+void loop() {
+    // Your main code
+}
